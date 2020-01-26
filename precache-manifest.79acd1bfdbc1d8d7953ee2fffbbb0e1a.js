80580d9f6b387f03a0b6f6604691aa3b84a33a8a
maxhha <37638564+maxhha@users.noreply.github.com>
2020-01-26 13:00:18 +0000

    Deploying to build from master b1888900d877ed8546054912ca3a5f6fa947279d

---
+++ precache-manifest.79acd1bfdbc1d8d7953ee2fffbbb0e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c317a193254b90bf73fdaeffbb7874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad344175558367b47a9",
+    "url": "/static/css/main.ca650a2f.chunk.css"
+  },
+  {
+    "revision": "d7cd89cb8d0ed5c1e185",
+    "url": "/static/js/2.cb052fdb.chunk.js"
+  },
+  {
+    "revision": "526188622d957293c1eeb74d1bba61eb",
+    "url": "/static/js/2.cb052fdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aad344175558367b47a9",
+    "url": "/static/js/main.6322568e.chunk.js"
+  },
+  {
+    "revision": "ed3651b5f194a35301f2",
+    "url": "/static/js/runtime-main.a755ef32.js"
+  }
+]);
